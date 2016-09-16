8536dffc2636bcbe9251254f5245a56a0b546e54
Fortune Soleil <fsoleil@stuy.edu>
2016-09-16 11:43:07 -0400

    Add files via upload

---
+++ euler2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+  int a = 1;
+  int b = 2;
+  int sum;
+  while (b < 4000000){
+    if !(b % 2){
+	sum += b;
+      }
+    b += a;
+    a = b - a;
+  }
+  printf("%d\n", sum);
+  return sum;
+}
